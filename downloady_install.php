7d168c832bcf94f6aa7f6864441ed3f06721efed
crestAT <a.schmidhuber@gmail.com>
2016-02-08 06:02:37 +0100

    0.1

--- downloady_install.php
+++ downloady_install.php
@@ -7,11 +7,13 @@ $release = explode("-", exec("uname -r"));
 if ($release[0] >= 9.3) $verify_hostname = "--no-verify-hostname";
 else $verify_hostname = "";
 
-$dirname = dirname(__FILE__);
-$return_val = mwexec("fetch {$verify_hostname} -vo {$dirname}/downloady/downloady-install.php 'https://raw.github.com/crestAT/nas4free-downloady/master/downloady/downloady-install.php'", true);
+$install_dir = dirname(__FILE__);                           // get directory where the installer script resides
+if (!is_dir("{$install_dir}/downloady")) { mkdir("{$install_dir}/downloady", 0775, true); }
+
+$return_val = mwexec("fetch {$verify_hostname} -vo {$install_dir}/downloady/downloady-install.php 'https://raw.github.com/crestAT/nas4free-downloady/master/downloady/downloady-install.php'", true);
 if ($return_val == 0) { 
-    chmod("{$dirname}/downloady/downloady-install.php", 0775);
-    require_once("{$dirname}/downloady/downloady-install.php"); 
+    chmod("{$install_dir}/downloady/downloady-install.php", 0775);
+    require_once("{$install_dir}/downloady/downloady-install.php"); 
 }
 else { echo "\nInstallation file 'downloady-install.php' not found, installation aborted!\n"; }
 ?>
